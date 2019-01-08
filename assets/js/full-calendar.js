29eae49a4fcbb290487c0c0099e18264d6ebc3aa
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2019-01-08 20:08:35 +0100

    adds full-calendar UI

---
+++ assets/js/full-calendar.js
@@ -0,0 +1,10 @@
+// import moment from 'moment';
+// import "moment/locale/fr";
+// moment.locale('fr');
+//
+// import $ from 'jquery';
+// import 'fullcalendar';
+//
+// require("fullcalendar/dist/fullcalendar.min.js");
+// require("fullcalendar/dist/fullcalendar.min.css");
+// require("fullcalendar/dist/locale-all.js")
