543ce91c70fa96609814ff1cd38962b68288163a
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2019-01-08 20:09:46 +0100

    adds others config and ui pages home and book

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,19 @@
+// import $ from 'jquery';
+// loads the jquery package from node_modules
+// var $ = require('jquery');
+
 // require('materialize-css');
-require('../sass/app.sass');
+require("materialize-css/dist/js/materialize.min.js");
+require("materialize-css/dist/css/materialize.min.css");
+require("../sass/app.sass");
 
 const imagesContext = require.context('../images', true, /\.(png|jpg|jpeg|gif|ico|svg|webp)$/);
 imagesContext.keys().forEach(imagesContext);
 
+//
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOMContentLoaded...');
+  
   M.Collapsible.init(document.querySelectorAll('.collapsible'));
   M.Sidenav.init(document.querySelectorAll('.sidenav'));
   M.FormSelect.init(document.querySelectorAll('select'));
