428749c34c1df634d8b036417f78c42b25a568f8
Youssouf SEKHARI <youssouf.sekhari@kabolt.fr>
2019-01-08 20:09:08 +0100

    add slick init && config

--- assets/js/slick-carousel.js
+++ assets/js/slick-carousel.js
@@ -1,6 +1,5 @@
 // import "../../node_modules/slick-carousel/slick/slick.js"
 
-require('jquery/dist/jquery.min.js');
 require('slick-carousel/slick/slick-theme.scss');
 require('slick-carousel/slick/slick.scss');
 require('slick-carousel/slick/slick.min.js');
